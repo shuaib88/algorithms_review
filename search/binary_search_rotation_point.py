d3bacb1d7d91dd8fe058cb01706c6ba9dc8eaa69
Shuaib Ahmed <shuaib.sva@gmail.com>
2017-08-25 16:31:06 -0500

    binary search practice

---
+++ search/binary_search_rotation_point.py
@@ -0,0 +1,46 @@
+
+def rotation_point_of(word_array, left_index=0, right_index =None):
+
+    if not right_index:
+        right_index = len(word_array) - 1
+
+    midpoint = (right_index + left_index)/2
+
+    if right_index - left_index == 0:
+        return midpoint
+
+    if word_array[midpoint]>word_array[midpoint+1]:
+        return midpoint
+
+    else:
+        if word_array[right_index]> word_array[midpoint]:
+            return rotation_point_of(word_array,left_index,midpoint)
+        else:
+            return rotation_point_of(word_array,midpoint,right_index)
+
+
+# test array
+word_array = [
+    'ptolemaic',
+    'retrograde',
+    'supplant',
+    'undulate',
+    'xenoepist',
+    'asymptote', # <-- rotates here!
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage',
+]
+# word_array = [
+#     'xenoepist',
+#     'asymptote', # <-- rotates here!
+# ]
+
+
+#execute
+# word_dict = known_word_dict(word_array)
+# word_dict.rotation_point_of(word_array)
+# print len(word_array)
+print rotation_point_of(word_array,right_index=len(word_array)-1)
